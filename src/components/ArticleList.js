015c90aedae41ea6994b8723a8a2757a44780a35
designerofthing <designerofthings@gmail.com>
2020-06-03 09:45:33 +0200

    removes import text

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ArticleCard from "../components/ArticleCard";
-import { FlatList, View, Text } from "react-native";
+import { FlatList, View } from "react-native";
 
 const ArticleList = () => {
   const [articleList, setArticleList] = useState([]);
