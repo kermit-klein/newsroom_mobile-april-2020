56af7b198483f0bc9870bbab7da693dff11f563e
pauline_barnades <barnades.pauline@gmail.com>
2020-06-04 12:07:04 +0200

    editing component, adding view

--- src/components/SingleArticle.js
+++ src/components/SingleArticle.js
@@ -1,8 +1,52 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { FlatList, View } from "react-native";
+import { View, Image } from "react-native";
 
-const SingleArticle = () => {
-  return <div>BAJS</div>;
+
+const SingleArticle = (props) => {
+  const [article, setArticle] = useState()
+  const { id } = useParams()
+
+  useEffect(() => {
+    const chooseArticle = async () => {
+      let response = await axios.get(`/articles/${id}`);
+      setArticle(response.data.article);
+    };
+    chooseArticle();
+  }, [])
+
+
+  return (
+
+    <View
+      key={article.id}
+      id={"article-" + article.id + "-title"}
+      className="article-title"
+    >
+      <text>{article.title}</text>
+    </View>
+  
+    <View
+      key={article.id}
+      id={"article-" + article.id + "-body"}
+      className="article-body"
+    >
+      <text>{article.body}</text>
+    </View>
+    <View
+      key={article.id}
+      id={"article-" + article.id + "-date"}
+      className="published-at"
+    >
+      <text>{('Published at')} {article.published_at}</text>
+    </View>
+    <Image
+      className={`article-image-${article.id}`}
+      source={{ uri: article.image }}
+      style={styles.image}
+    />
+  
+  
+  );
 };
 export default SingleArticle;
