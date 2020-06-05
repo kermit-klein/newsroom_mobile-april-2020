85125763e0a5f6fa0813b65f186065c532ed8556
designerofthing <designerofthings@gmail.com>
2020-06-05 22:46:43 +0200

    adds test for other category

--- cypress/integration/visitorCanFilterArticlesByCategory.feature.js
+++ cypress/integration/visitorCanFilterArticlesByCategory.feature.js
@@ -29,13 +29,28 @@ describe("Visitor can filter by category", () => {
       url: "http://localhost:3000/api/articles",
       response: "fixture:article_economy_cat_list.json",
     });
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/articles",
+      response: "fixture:article_other_cat_list.json",
+    });
     cy.viewport("samsung-s10");
     cy.visit("/");
   });
 
-  it("economy successfully", () => {
+  it("economy category successfully", () => {
     cy.contains("Category").click();
     cy.get("div").contains("Economy").click();
+    cy.get("#article-title-1").should("be.visible");
+    cy.get("#article-title-2").should("be.visible");
+    cy.get("#article-title-3").scrollIntoView().should("be.visible");
+    cy.get("#article-title-4").should("be.visible");
+    cy.get("#article-title-5").should("be.visible");
+  });
+
+  it("other category successfully", () => {
+    cy.contains("Category").click();
+    cy.get("div").contains("Other").click();
     cy.get("#article-title-1").should("be.visible");
     cy.get("#article-title-2").should("be.visible");
     cy.get("#article-title-3").scrollIntoView().should("be.visible");
